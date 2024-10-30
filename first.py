6055514d16e116921ceb2292cda2e2a0f69797f4
Tariel22 <gagnit00@jcu.cz>
2024-10-30 14:08:45 +0100

    Create first.py

---
+++ first.py
@@ -0,0 +1,9 @@
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print(f"Число {cislo} - чётное")
+    else:
+        print(f"Число {cislo} - нечётное")
+
+# Вызов функции
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)
